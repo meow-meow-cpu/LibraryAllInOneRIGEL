202f47dcd3c6ed3933c9b2da002b08c8a4df497f
Emily_Yang <em.ek.yang@gmail.com>
2023-04-14 16:48:57 -0400

    US03_EY

--- src/test/java/com/library/steps/US03StepDefs_EY.java
+++ src/test/java/com/library/steps/US03StepDefs_EY.java
@@ -5,6 +5,7 @@ import com.library.utility.LibraryAPI_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -20,16 +21,19 @@ public class US03StepDefs_EY {
     ValidatableResponse thenPart;
 
 
+    /*
     @Given("I logged Library api as a {string}")
     public void i_logged_library_api_as_a(String userType) {
         givenPart = given().log().uri()
                 .header("x-library-token", LibraryAPI_Util.getToken(userType));
     }
 
+
     @Given("Accept header is {string}")
     public void accept_header_is(String contentType) {
         givenPart.accept(contentType);
     }
+     */
 
     @Given("Request Content Type header is {string}")
     public void request_content_type_header_is(String contentTypeHeader) {
@@ -47,6 +51,7 @@ public class US03StepDefs_EY {
         thenPart = response.then();
     }
 
+    /*
     @Then("status code should be {int}")
     public void status_code_should_be(Integer statusCode) {
         thenPart.statusCode(statusCode);
@@ -56,14 +61,17 @@ public class US03StepDefs_EY {
     public void response_content_type_is(String contentType) {
         thenPart.contentType(contentType);
     }
+     */
 
     @Then("the field value for {string} path should be equal to {string}")
     public void the_field_value_for_path_should_be_equal_to(String path, String message) {
         thenPart.body(path, is(message));
     }
 
+    /*
     @Then("{string} field should not be null")
     public void field_should_not_be_null(String path) {
         thenPart.body(path, everyItem(notNullValue()));
     }
+     */
 }
