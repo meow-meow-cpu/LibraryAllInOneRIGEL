7890359194e129e7ccd36c3fce9edcd9c7403fbb
Emily_Yang <em.ek.yang@gmail.com>
2023-04-14 17:22:10 -0400

    US03_EY

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -2,6 +2,7 @@ package com.library.steps;
 
 import com.library.utility.ConfigurationReader;
 import com.library.utility.LibraryAPI_Util;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,6 +12,9 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -53,4 +57,49 @@ public class APIStepDefs {
         thenPart.body(path, everyItem(notNullValue()));
     }
 
+
+    // -----US03-----
+
+    @And("Request Content Type header is {string}")
+    public void requestContentTypeHeaderIs(String contentType) {
+        givenPart.contentType(contentType);
+    }
+
+    Map<String, Object> randomBook;
+    @Given("I create a random {string} as request body")
+    public void i_create_a_random_as_request_body(String random) {
+        if (random.equals("book")){
+            this.randomBook = LibraryAPI_Util.getRandomBookMap();
+        }else{
+            System.exit(1);   // yes?
+        }
+    }
+
+    @When("I send POST request to {string} endpoint")
+    public void i_send_post_request_to_endpoint(String post) {
+        response = givenPart.when().post(ConfigurationReader.getProperty("library.baseUri") + post).prettyPeek();
+        thenPart = response.then();
+    }
+
+    @Then("the field value for {string} path should be equal to {string}")
+    public void the_field_value_for_path_should_be_equal_to(String path, String message) {
+        Assert.assertEquals(response.path(path), message);
+    }
+
+    @And("I logged in Library UI as {string}")
+    public void iLoggedInLibraryUIAs(String user) {
+
+    }
+
+
+    @And("I navigate to {string} page")
+    public void iNavigateToPage(String page) {
+
+    }
+
+
+    @And("UI, Database and API created book information must match")
+    public void uiDatabaseAndAPICreatedBookInformationMustMatch() {
+
+    }
 }
