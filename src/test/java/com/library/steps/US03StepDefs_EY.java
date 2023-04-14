2ae1e82fbb064b12e5347ccbca93ddc0146e2633
Emily_Yang <em.ek.yang@gmail.com>
2023-04-14 11:17:50 -0400

    US03_EY started

---
+++ src/test/java/com/library/steps/US03StepDefs_EY.java
@@ -0,0 +1,72 @@
+package com.library.steps;
+
+import com.library.utility.ConfigurationReader;
+import com.library.utility.LibraryAPI_Util;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class US03StepDefs_EY {
+
+    RequestSpecification givenPart;
+    Response response;
+    ValidatableResponse thenPart;
+    /**
+     * US 01 RELATED STEPS
+     *
+     */
+    @Given("I logged Library api as a {string}")
+    public void i_logged_library_api_as_a(String userType) {
+
+        givenPart = given().log().uri()
+                .header("x-library-token", LibraryAPI_Util.getToken(userType));
+    }
+
+    @Given("Accept header is {string}")
+    public void accept_header_is(String contentType) {
+        givenPart.accept(contentType);
+    }
+
+    @Given("Request Content Type header is {string}")
+    public void request_content_type_header_is(String contentTypeHeader) {
+
+    }
+
+    @Given("I create a random {string} as request body")
+    public void i_create_a_random_as_request_body(String book) {
+
+    }
+
+    @When("I send POST request to {string} endpoint")
+    public void i_send_post_request_to_endpoint(String endpoint) {
+        response = givenPart.when().get(ConfigurationReader.getProperty("library.baseUri") + endpoint).prettyPeek();
+        thenPart = response.then();
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(Integer statusCode) {
+        thenPart.statusCode(statusCode);
+    }
+
+    @Then("Response Content type is {string}")
+    public void response_content_type_is(String contentType) {
+        thenPart.contentType(contentType);
+    }
+
+    @Then("the field value for {string} path should be equal to {string}")
+    public void the_field_value_for_path_should_be_equal_to(String fieldValue, String message) {
+
+    }
+
+    @Then("{string} field should not be null")
+    public void field_should_not_be_null(String path) {
+        thenPart.body(path, everyItem(notNullValue()));
+    }
+}
